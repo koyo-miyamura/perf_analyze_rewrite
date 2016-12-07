909046c16dca3110a28b8d81aea8ee077c41f60f
koyo <koyo.miyamura@cpc.ait.kyushu-u.ac.jp>
2016-12-07 16:02:29 +0900

    Delete count_test.sh

--- count_test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-SCRDIR=$(readlink -f `dirname $0`)
-ANALYZE=${SCRDIR}/perf_analyze_count.py 
-cd ..
-${ANALYZE} /home/koyo/perftool_timeseries-master_byme/perftool_timeseries-master/perf.data.dir/../perf.csv/20161206170319 4
